c387e9eb54872170875c60d85217fd3e683e4d53
vodichka671 <vodichka671@gmail.com>
2019-04-04 19:59:24 +0300

    Model + Controller stuff

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Random rnd = new Random();
+
+     //   System.out.println(rnd.nextInt(100-5)+5);
+
     }
 }
